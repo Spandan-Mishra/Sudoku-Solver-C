42b5bb512ba21caf01bd2c5a2d2c46e810f4560b
spandev <143828642+Spandan-Mishra@users.noreply.github.com>
2024-06-07 02:21:40 +0530

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -10,7 +10,8 @@
 
 int grid[length][length];
 bool success = false;
-bool *successfull = &success;   
+bool *successfull = &success;
+// sudoku grid for checking
 // 5 3 0 0 7 0 0 0 0
 // 6 0 0 1 9 5 0 0 0
 // 0 9 8 0 0 0 0 6 0
